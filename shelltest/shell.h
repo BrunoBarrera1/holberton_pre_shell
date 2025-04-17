5681dced6540a3781205187ce74c4534bef18325
federico-paganini <perfedefer@gmail.com>
2025-04-17 20:22:44 +0000

    [FEAT ] shell.h

---
+++ shelltest/shell.h
@@ -0,0 +1,13 @@
+#ifndef _GATES_OF_SHELL_H
+#define _GATES_OF_SHELL_H
+
+/* Libraries imported to whole project */
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_line(void);
+char **check_validity(char *);
+
+#endif
